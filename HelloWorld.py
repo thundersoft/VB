09806b7dedf31f3f468915c0afef5be7e9e982df
Srikanth <thundertest11@yahoo.com>
2022-09-06 21:24:03 +0530

    added hello world program

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+def hello_world():
+   print ("HELLO WORLD!")
+   
+def subtraction(num1, num2):
+   sub1 = int(num1)-int(num2)
+   print sub1
+   
+def hello_world1():
+   print ("HELLO WORLD111!")
+   
+def join_two_strings(arg1, arg2):
+    print arg1 + " " + arg2
+    return arg1 + " " + arg2
